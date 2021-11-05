c6ea5bdbdefce91f42347ebbf916b7276f145956
Luan Felipe Rodrigues Sanches <49524975+LuanFRSanches@users.noreply.github.com>
2021-11-05 12:46:23 -0400

    Executando a operação de reduction em uma stream

---
+++ lambda-stream-api/src/lambda/stream/api/reduction/Main.java
@@ -0,0 +1,20 @@
+package lambda.stream.api.reduction;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = List.of(3, 2, 5).stream().mapToInt(Integer::intValue).toArray();
+        System.out.println(multiply(array));
+        System.out.println(max(array));
+    }
+
+    private static int multiply(int[] array) {
+        return Arrays.stream(array).reduce((x, y) -> x * y).orElse(0);
+    }
+
+    private static int max(int[] array) {
+        return Arrays.stream(array).reduce((x, y) -> x > y ? x : y).orElse(0);
+    }
+}
